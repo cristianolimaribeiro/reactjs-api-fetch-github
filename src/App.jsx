86640ec18db65d03e87a8337f86effa0ae224d21
cristianolimaribeiro <cristiano.lima.ribeiro@gmail.com>
2022-03-19 09:43:05 -0300

    aplicação fetch api github

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Repositorylist } from './components/RepositoryList';
+import './styles/global.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Repositorylist />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,27 @@
+﻿import {useState , useEffect} from 'react'
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss'
+
+const API_URL = 'https://api.github.com/users/cristianolimaribeiro/repos'
+export function Repositorylist(){
+
+    const [repositories, setRepositories] = useState([]); 
+
+    useEffect(() => {
+        fetch(`${API_URL}`)
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+            </ul>
+        </section>
+    )
+}
